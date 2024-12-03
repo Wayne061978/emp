e33628dd09fe6198a5d85321efc4c5a06ed03bde
Jacob Moore <jacob.robert.moore@gmail.com>
2024-12-03 08:20:56 -0800

    updated commission

--- src/CommEmp.java
+++ src/CommEmp.java
@@ -1,6 +1,7 @@
 public class CommEmp extends Employee {
 
     private double sales;
+    private double commissionRate = .10;
 
     public CommEmp(String firstName, String lastName, String title, double payRate, double sales){
         super(firstName, lastName, title, payRate);
@@ -9,13 +10,19 @@ public class CommEmp extends Employee {
 
     @Override
     public double calculateSalary() {
-        return payRate + (sales * .10);
+        if (sales > 1000) {
+            return payRate + (sales * commissionRate);
+        } else {
+            return payRate;
+        }
     }
     public void display(){
         System.out.println(super.toString());
-        double commission = sales * 0.10;
-        System.out.printf("Commission: %.2f%n", commission);
-        System.out.printf("Weekly pay: %.2f%n", calculateSalary());
+        double commission = (sales >= 1000) ? (sales * commissionRate) : 0;
+        if (commission > 0) {
+            System.out.printf("Commission: %.2f%n", commission);
+        }
+        System.out.printf("Weekly pay: %.2f%n", (calculateSalary() + commission));
     }
 
 }
